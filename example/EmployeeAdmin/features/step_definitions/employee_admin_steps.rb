61615d044705d662478b79c04b10c435c0abca8a
Derek Longmuir <derek.longmuir@gmail.com>
2011-02-25 04:23:41 -0800

    Update example to use gem. Fix some formatting

--- example/EmployeeAdmin/features/step_definitions/employee_admin_steps.rb
+++ example/EmployeeAdmin/features/step_definitions/employee_admin_steps.rb
@@ -14,15 +14,15 @@ Given /^test user Brian is present$/ do
   steps <<EOS 
     Given I should be on the Users screen
     When I touch the Add User button
-		And I fill in text fields as follows:
-		  | field      | text   |
-			| First Name | Brian  |
-			| Last Name  | Knorr  |
-			| Email      | a@b.c  |
-			| Username   | bknorr |
-			| Password   | 123    |
-			| Confirm    | 123    |
-		And I touch "Save"
-		Then I should be on the Users screen
+    And I fill in text fields as follows:
+      | field      | text   |
+      | First Name | Brian  |
+      | Last Name  | Knorr  |
+      | Email      | a@b.c  |
+      | Username   | bknorr |
+      | Password   | 123    |
+      | Confirm    | 123    |
+    And I touch "Save"
+    Then I should be on the Users screen
 EOS
 end
