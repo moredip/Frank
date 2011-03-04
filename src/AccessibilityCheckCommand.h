74a20fa8877260d86d4841eba3c38dc3979af318
Pete Hodgson <git@thepete.net>
2011-03-04 15:46:07 -0800

    Check that accessibility is enabled on the device when app is initially launched

---
+++ src/AccessibilityCheckCommand.h
@@ -0,0 +1,15 @@
+//
+//  AccessibilityCheckCommand.h
+//  Chase iPad
+//
+//  Created by Pete Hodgson on 1/19/11.
+//  Copyright 2011 JPMorgan Chase. All rights reserved.
+//
+
+#import "FrankCommandRoute.h"
+
+@interface AccessibilityCheckCommand : NSObject<FrankCommand> {
+	
+}
+
+@end
