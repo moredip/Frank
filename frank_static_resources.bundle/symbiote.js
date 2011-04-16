287d763a30bd06a31ee5c2e558b8ee2ceb7e13fa
Pete Hodgson <git@thepete.net>
2011-04-16 16:10:31 -0700

    WIP on cleaning up accessible-views tab

--- frank_static_resources.bundle/symbiote.js
+++ frank_static_resources.bundle/symbiote.js
@@ -5,7 +5,7 @@ var G = {
 
 var Symbiote = {
   is_error_response: function( response ){
-    return 'ERROR' == response.outcome;
+    return 'ERROR' === response.outcome;
   },
   display_error_response: function( response ){
     alert( "Frank isn't happy: "+response.reason+"\n"
@@ -19,29 +19,6 @@ var Symbiote = {
     $('#loading').hide();
   }
 };
-
-(function(){
-  function transformDumpedViewToListItem( rawView ) {
-    var title = ""+rawView['class'];
-    if( rawView['accessibilityLabel'] )
-      title = title + ": '"+rawView['accessibilityLabel']+"'";
-
-    var viewListItem = $("<li><a>"+title+"</a></li>"),
-    subviewList = $("<ul/>");
-
-    $('a',viewListItem).data( 'rawView', rawView );
-
-    $.each( rawView.subviews, function(i,subview) {
-      subviewList.append( transformDumpedViewToListItem( subview ) );
-    });
-    
-    viewListItem.append( subviewList );
-    return viewListItem; 
-  };
-
-  Symbiote.transformDumpedViewToListItem = transformDumpedViewToListItem;
-
-}());
 
 function classClicked(link){    
     var command = {
@@ -76,7 +53,41 @@ $(document).ready(function() {
 
   var $domDetails = $('#dom_detail'),
       $domList = $('div#dom_dump > ul'),
+      $domAccessibleDump = $('div#accessible-views'),
       INTERESTING_PROPERTIES = ['class', 'accessibilityLabel', 'tag', 'alpha', 'isHidden'];
+
+
+
+
+  function sendFlashCommand( selector ) {
+    var command = {
+      query: selector,
+      operation: {
+        method_name: 'flash',
+        arguments: []
+      }
+    };
+
+    Symbiote.display_chatting_popup();
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify( command ),
+      url: G.base_url + "/map",
+      success: function(data) {
+        if( Symbiote.is_error_response( data ) )
+          Symbiote.display_error_response( data );
+      },
+      error: function(xhr,status,error) {
+        alert( "Error while talking to Frank: " + status );
+        $('#loading').hide();
+      },
+      complete: function(xhr,status) {
+        Symbiote.hide_chatting_popup();
+      }
+    });
+  }
+
 
 
   function displayDetailsFor( view ) {
@@ -109,7 +120,7 @@ $(document).ready(function() {
 
     _.each( _.keys(view).sort(), function(propertyName) {
       if( propertyName == 'subviews' ){ return }
-      if( _.contains( INTERESTING_PROPERTIES, propertyName ) ){ return } // don't want to include the interesting properties twice
+      if( _.contains( INTERESTING_PROPERTIES, propertyName ) ){ return; } // don't want to include the interesting properties twice
 
       var propertyValue = view[propertyName];
       $table.append( tableRow( propertyName, propertyValue ) );
@@ -129,6 +140,67 @@ $(document).ready(function() {
   }
 
 
+  function transformDumpedViewToListItem( rawView ) {
+    var title = ""+rawView['class'];
+    if( rawView.accessibilityLabel )
+      title = title + ": '"+rawView.accessibilityLabel+"'";
+
+    var viewListItem = $("<li><a>"+title+"</a></li>"),
+    subviewList = $("<ul/>");
+
+    $('a',viewListItem).data( 'rawView', rawView );
+
+    $.each( rawView.subviews, function(i,subview) {
+      subviewList.append( transformDumpedViewToListItem( subview ) );
+    });
+    
+    viewListItem.append( subviewList );
+    return viewListItem; 
+  };
+
+
+  function updateDumpView( data ) {
+    $domList.children().remove();
+    $domList.append( transformDumpedViewToListItem( data ) );
+    $('a', $domList ).bind( 'click', treeElementSelected );
+    $domList.treeview({
+                 collapsed: false
+                 });
+  }
+
+  function collectAccessibleViews( view )
+  {
+    var accessibleSubElements = _.flatten( _.map( view.subviews, function(subview) {
+      return collectAccessibleViews( subview );
+    }) );
+
+    if( view.accessibilityLabel ) {
+      return [view].concat( accessibleSubElements );
+    }else{
+      return accessibleSubElements;
+    }
+  }
+
+  function selectorForAccessibleView( view ) {
+    return "view:'"+view.class+"' marked:'" + view.accessibilityLabel + "'";
+  }
+
+  function updateAccessibleViews( data ) {
+    var accessibleViews = collectAccessibleViews( data );
+        	
+    $domAccessibleDump.children().remove();
+
+    _.each( accessibleViews, function( view ) {
+      var selector = selectorForAccessibleView(view);
+      $('<div><a href="#" title="'+selector+'"><span class="viewClass">'+view.class+'</span> with label "<span class="viewLabel">'+view.accessibilityLabel+'</span>"</a></div>')
+        .click( function(){
+          sendFlashCommand( selector );
+          return false;
+        })
+        .appendTo( $domAccessibleDump );
+    });
+  }
+
 	$("#tabs").tabs();
 	$('#loading').hide();
 	$('#dump_button').click( function(){
@@ -141,75 +213,19 @@ $(document).ready(function() {
       url: G.base_url + "/dump",
       success: function(data) {
         console.debug( 'dump returned', data );
-
-        $domList.children().remove();
-        $domList.append( Symbiote.transformDumpedViewToListItem( data ) );
-        $('a', $domList ).bind( 'click', treeElementSelected );
-        $domList.treeview({
-                     collapsed: false
-                     });
-		 Symbiote.hide_chatting_popup();						   
+        updateDumpView( data );
+        updateAccessibleViews( data );
+        Symbiote.hide_chatting_popup();						   
       },
       error: function(xhr,status,error) {
         $('#loading').hide();
         alert( "Error while talking to Frank: " + status );
       }
     });
-    
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      data: '["DUMMY"]', // a bug in cocoahttpserver means it can't handle POSTs without a body
-      url: G.base_url + "/inspect",
-      success: function(data) {
-		$('#loading').hide();
-		var test = eval(data);
-		var html = '<table id="accessibility">';
-        html +=	'<tr><th>Marked (accessibilityLabel)</th><th>Class</th></tr>';
-        	
-		for (index in test)
-  		{
-  			var elem = test[index];
-  			html += '<tr><td><a onClick="javascript: classClicked(this);" href="#">' + elem.label+ '</a></td><td>' + elem.class + '</td></tr>';
-  		}
-		html += '</table>';
-		$('div#access_dump').html(html);
-      },
-      error: function(xhr,status,error) {
-      $('#loading').hide();
-        alert( "Error while talking to Frank: " + status );
-      }
-    });
-    
   });
 
   $('#flash_button').click( function(){
-    var command = {
-      query: $("input#query").val(),
-      operation: {
-        method_name: 'flash',
-        arguments: []
-      }
-    };
-
-    Symbiote.display_chatting_popup();
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      data: JSON.stringify( command ),
-      url: G.base_url + "/map",
-      success: function(data) {
-        if( Symbiote.is_error_response( data ) )
-          Symbiote.display_error_response( data );
-      },
-      error: function(xhr,status,error) {
-        alert( "Error while talking to Frank: " + status );
-        $('#loading').hide();
-      },
-      complete: function(xhr,status) {
-        Symbiote.hide_chatting_popup();
-      }
-    });
+    sendFlashCommand( $("input#query").val() );
   });
   
   // do initial DOM dump straight after page has finished loading
