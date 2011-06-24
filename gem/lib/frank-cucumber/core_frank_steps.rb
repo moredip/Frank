224bb5da534211efa8cb30ac1cf5c21a36644ebc
pdkn <keanepaddy@gmail.com>
2011-06-24 08:10:27 -0700

    Edited gem/lib/frank-cucumber/core_frank_steps.rb via GitHub

--- gem/lib/frank-cucumber/core_frank_steps.rb
+++ gem/lib/frank-cucumber/core_frank_steps.rb
@@ -71,7 +71,11 @@ Then /^I should see a navigation bar titled "([^\"]*)"$/ do |expected_mark|
   values.should include(expected_mark)
 end
 
-Then /^I should see an alert view titled "([^\"]*)"$/ do |expected_mark|
+Then /^I should see an alert view with title "([^\"]*)"$/ do |expected_mark|
+  check_element_exists( "alertView view marked:'#{expected_mark}'" )
+end
+
+Then /^I should see an alert view with message "([^\"]*)"$/ do |expected_mark|
   values = frankly_map( 'alertView', 'message')
   puts values
   values.should include(expected_mark)
