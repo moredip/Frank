e3cf162336f8c795a1f581cfe7bcb767de4b3100
pdkn <keanepaddy@gmail.com>
2011-06-24 09:45:32 -0700

    breaking out SDK version

--- gem/features-skeleton/support/env.rb
+++ gem/features-skeleton/support/env.rb
@@ -1,5 +1,9 @@
 require 'frank-cucumber'
 
-SDK    = "4.3.2"
+SDK_VERSION_MAJOR     = "4"
+SDK_VERSION_MINOR     = ".3"
+SDK_VERSION_INCREMENT = ".2"
+
 APPNAME = "APP_NAME" #replace with your app name
-APPLICATIONS_DIR = "#{ENV['HOME']}/Library/Application Support/iPhone Simulator/#{SDK}/Applications"
+
+APPLICATIONS_DIR = "#{ENV['HOME']}/Library/Application Support/iPhone Simulator/#{SDK_VERSION_MAJOR}#{SDK_VERSION_MINOR}#{SDK_VERSION_INCREMENT}/Applications"
