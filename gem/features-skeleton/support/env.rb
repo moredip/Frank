c901eb7a6a40e6ba8d73eba23d1b4b7dbbcafc4a
pdkn <keanepaddy@gmail.com>
2011-06-24 09:36:23 -0700

    amend app name placeholder

--- gem/features-skeleton/support/env.rb
+++ gem/features-skeleton/support/env.rb
@@ -1,5 +1,5 @@
 require 'frank-cucumber'
 
 SDK    = "4.3.2"
-APPNAME = "NTC"
+APPNAME = "APP_NAME" #replace with your app name
 APPLICATIONS_DIR = "#{ENV['HOME']}/Library/Application Support/iPhone Simulator/#{SDK}/Applications"
