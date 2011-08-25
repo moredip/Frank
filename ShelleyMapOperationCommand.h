ead0cd2c4815888708b6e2c5ba081c80560f442a
Pete Hodgson <git@thepete.net>
2011-08-24 20:53:30 -0700

    add Shelley to Frank, with experimental shelly_map operation

---
+++ ShelleyMapOperationCommand.h
@@ -0,0 +1,16 @@
+//
+//  ShelleyMapOperationCommand.h
+//  Frank
+//
+//  Created by Pete Hodgson on 8/24/11.
+//  Copyright 2011 ThoughtWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FrankCommandRoute.h"
+
+@interface ShelleyMapOperationCommand : NSObject<FrankCommand> {
+}
+
+@end
--- src/FrankServer.m
+++ src/FrankServer.m
@@ -14,6 +14,7 @@
 #import "FrankCommandRoute.h"
 #import "DumpCommand.h"
 #import "MapOperationCommand.h"
+#import "ShelleyMapOperationCommand.h"
 #import "OrientationCommand.h"
 #import "AppCommand.h"
 #import "AccessibilityCheckCommand.h"
@@ -35,6 +36,7 @@
 		FrankCommandRoute *frankCommandRoute = [[[FrankCommandRoute alloc] init] autorelease];
 		[frankCommandRoute registerCommand:[[[DumpCommand alloc]init]autorelease] withName:@"dump"];
 		[frankCommandRoute registerCommand:[[[MapOperationCommand alloc]init]autorelease] withName:@"map"];
+		[frankCommandRoute registerCommand:[[[ShelleyMapOperationCommand alloc]init]autorelease] withName:@"shelley_map"];
 		[frankCommandRoute registerCommand:[[[OrientationCommand alloc]init]autorelease] withName:@"orientation"];
 		[frankCommandRoute registerCommand:[[[AccessibilityCheckCommand alloc] init]autorelease] withName:@"accessibility_check"];
 		[frankCommandRoute registerCommand:[[[AppCommand alloc] init]autorelease] withName:@"app_exec"];
