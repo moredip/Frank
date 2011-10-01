0d2a3973c4322340c8a9fbfe3e4740255da3f2bd
Pete Hodgson <git@thepete.net>
2011-09-30 22:20:43 -0700

    Initial commit of Controls example app

---
+++ example/Controls/Controls/ControlsAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ControlsAppDelegate.h
+//  Controls
+//
+//  Created by Pete Hodgson on 9/30/11.
+//  Copyright 2011 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControlsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
