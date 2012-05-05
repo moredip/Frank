8417703824ab6ab429a7db2af99bf90a488d346d
Pete Hodgson <git@thepete.net>
2012-05-05 15:39:16 -0700

    fix up gem

--- example/Controls/features/step_definitions/navigation_steps.rb
+++ example/Controls/features/step_definitions/navigation_steps.rb
@@ -1,11 +1,6 @@
 Then /^I should be in the "([^"]*)" view$/ do |expected_view_title|
   expected_mark = "view:'UINavigationItemView' marked:'#{expected_view_title}'"
-
-  Timeout::timeout(WAIT_TIMEOUT) do
-    until view_with_mark_exists( expected_mark )
-      sleep 0.1
-    end
-  end
+  wait_for_element_to_exist expected_mark
 end
 
 When /^I ask the app to reset to home$/ do
