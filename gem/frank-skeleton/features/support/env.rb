f019656da4fd382556dd91f855137b6f3aebe943
Pete Hodgson <git@thepete.net>
2012-05-13 17:40:27 -0400

    use shelley by default

--- gem/frank-skeleton/features/support/env.rb
+++ gem/frank-skeleton/features/support/env.rb
@@ -1,5 +1,8 @@
 require 'frank-cucumber'
 
+# UIQuery is deprecated. Please use the shelley selector engine. 
+Frank::Cucumber::FrankHelper.use_shelley_from_now_on
+
 # TODO: set this constant to the full path for your Frankified target's app bundle.
 # See the "Given I launch the app" step definition in launch_steps.rb for more details
 APP_BUNDLE_PATH = nil
