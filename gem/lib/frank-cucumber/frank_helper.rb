5920e4313c763479fbcfa5bf24b6f1f81438ba84
Pete Hodgson <git@thepete.net>
2012-05-13 22:05:24 -0500

    move host scripting stuff into seperate module

--- gem/lib/frank-cucumber/frank_helper.rb
+++ gem/lib/frank-cucumber/frank_helper.rb
@@ -1,12 +1,13 @@
 require 'json'
 require 'frank-cucumber/gateway'
-require 'frank-cucumber/frank_localize'
+require 'frank-cucumber/host_scripting'
 require 'frank-cucumber/wait_helper'
 
 module Frank module Cucumber
 
 module FrankHelper 
   include WaitHelper
+  include HostScripting
 
   class << self
     # TODO: adding an ivar to the module itself is a big ugyl hack. We need a FrankDriver class, or similar
@@ -203,83 +204,6 @@ module FrankHelper
     @_frank_server ||= Frank::Cucumber::Gateway.new
   end
  
-
-
-  
-  def start_recording
-    %x{osascript<<APPLESCRIPT
-	tell application "QuickTime Player"
-	set sr to new screen recording
-	  tell sr to start
-	end tell
-  APPLESCRIPT}
-  
-  end
-
-  def stop_recording
-    %x{osascript<<APPLESCRIPT
-	tell application "QuickTime Player"
-	  set sr to (document 1)
-	  tell sr to stop
-	end tell
-  APPLESCRIPT}  
-  end
-  
-  def quit_simulator
-    %x{osascript<<APPLESCRIPT-
-      application "iPhone Simulator" quit
-    APPLESCRIPT}
-  end
-
-def simulator_reset_data
-  %x{osascript<<APPLESCRIPT
-activate application "iPhone Simulator"
-tell application "System Events"
-  click menu item 5 of menu 1 of menu bar item 2 of menu bar 1 of process "#{Localize.t(:iphone_simulator)}"
-  delay 0.5
-  click button 2 of window 1 of process "#{Localize.t(:iphone_simulator)}"
-end tell
-  APPLESCRIPT} 
-end
-
-  #Note this needs to have "Enable access for assistive devices"
-  #chcked in the Universal Access system preferences
-  def simulator_hardware_menu_press( menu_label )
-    %x{osascript<<APPLESCRIPT
-activate application "iPhone Simulator"
-tell application "System Events"
-	click menu item "#{menu_label}" of menu "#{Localize.t(:hardware)}" of menu bar of process "#{Localize.t(:iphone_simulator)}"
-end tell
-  APPLESCRIPT}  
-  end
-  
-  def press_home_on_simulator
-    simulator_hardware_menu_press Localize.t(:home)
-  end
-  
-  def rotate_simulator_left
-    simulator_hardware_menu_press Localize.t(:rotate_left)
-  end
-
-  def rotate_simulator_right
-    simulator_hardware_menu_press Localize.t(:rotate_right)
-  end
-
-  def shake_simulator
-    simulator_hardware_menu_press Localize.t(:shake_gesture)
-  end
-  
-  def simulate_memory_warning
-    simulator_hardware_menu_press Localize.t(:simulate_memory_warning)
-  end
-  
-  def toggle_call_status_bar
-    simulator_hardware_menu_press Localize.t(:toggle_call_status_bar)
-  end
-  
-  def simulate_hardware_keyboard
-    simulator_hardware_menu_press Localize.t(:simulate_hardware_keyboard)
-  end
 end
 
 
