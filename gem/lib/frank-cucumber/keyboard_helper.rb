550b8f0fef521d8684666a8b64d623ccb12c95c0
Pete Hodgson <git@thepete.net>
2012-05-22 06:04:03 -0700

    add KeyboardHelper to gem

---
+++ gem/lib/frank-cucumber/keyboard_helper.rb
@@ -0,0 +1,14 @@
+module Frank
+module Cucumber
+
+module KeyboardHelper
+  def type_into_keyboard(text_to_type)
+    text_to_type = text_to_type+"\n" unless text_to_type.end_with?("\n")
+    res = frank_server.send_post( 
+      'type_into_keyboard',
+      :text_to_type => text_to_type,
+    )
+    Frank::Cucumber::Gateway.evaluate_frankly_response( res, "typing the following into the keyboard '#{text_to_type}'" )
+  end
+end
+end end
