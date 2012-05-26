b1c8d1ff2d8c711a3b366b222134b761666dacf1
Pete Hodgson <git@thepete.net>
2012-05-25 23:01:24 -0700

    getting example specs running on physical device

--- example/Controls/features/support/env.rb
+++ example/Controls/features/support/env.rb
@@ -6,5 +6,9 @@ $USING_PHYSICAL_DEVICE = !!ENV['USE_PHYSICAL_DEVICE']
 
 if $USING_PHYSICAL_DEVICE
   Frank::Cucumber::FrankHelper.test_on_physical_device_via_bonjour
+
+  After do
+    step 'I ask the app to reset to home'
+  end
 end
 
