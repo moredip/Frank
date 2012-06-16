04f5c87d802ac0b56975df929df3649a22068256
Pete Hodgson <git@thepete.net>
2012-06-16 11:01:01 -0700

    cleanup cli.rb

--- gem/lib/frank-cucumber/cli.rb
+++ gem/lib/frank-cucumber/cli.rb
@@ -4,6 +4,10 @@ module Frank
   class CLI < Thor
     include Thor::Actions
 
+    def self.source_root
+      File.join( File.dirname(__FILE__), '..','..','frank-skeleton' )
+    end
+
     desc "skeleton", "set up your iOS app by adding a Frank subdirectory containing everything Frank needs"
     def skeleton
       directory ".", "Frank"
@@ -23,30 +27,6 @@ module Frank
       end
       directory( 'frank_static_resources.bundle', 'Frank/frank_static_resources.bundle', :force => true )
     end
-
-
-    def self.source_root
-      File.join( File.dirname(__FILE__), '..','..','frank-skeleton' )
-    end
-
-    private
-
-
-#def update_server_files
-  #source_files = %w{libFrank.a libShelley.a frank_static_resources.bundle}.map{ |x| File.join( SOURCE_DIR, x ) }
-  #FileUtils.cp_r( source_files, TARGET_DIR )
-  #puts "All done. I've updated libFrank.a, libShelley.a and frank_static_resources.bundle inside #{TARGET_DIR}"
-#end
-
-#frank_dir_already_exists = File.exists?( TARGET_DIR ) 
-
-#if frank_dir_already_exists && !update_mode
-  #puts "A Frank subdirectory already exists. I don't want to mess with that. \n\nIf you want me to update the frank server code in that directory then run `frank-skeleton update-server`"
-  #exit 1
-#elsif !frank_dir_already_exists && update_mode
-  #puts "There isn't a Frank subdirectory here for me to update.\n\nIf you want to create a new Frank subdirectory containing the Frank server code and an initial Cucumber setup then you should run `frank-skeleton` with no arguments"
-  #exit 2
-#end
 
   end
 
