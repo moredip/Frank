380d1c072228a4308465fe28ad75d4b1d387e4de
Pete Hodgson <git@thepete.net>
2012-06-28 18:25:06 -0700

    tweak/cleanup of swipe gesture code

--- src/UIView+UIAutomationBridge.m
+++ src/UIView+UIAutomationBridge.m
@@ -29,31 +29,33 @@ UIASyntheticEvents *events(){
     return [NSValue valueWithCGPoint:tapPoint];
 }
 
-
-//returns what portion of the view to swipe along in the x and y axes.
-//we always include at least a small component in each axes because in the past totally 'right-angled'
+// THESE MAGIC NUMBERS ARE IMPORTANT. From experimentation it appears that too big or too small a ration leads to 
+// gestures not being recognized as such by the system. For example setting the big ratio to 0.4 leads to 
+// swipe-to-delete not working on UITableViewCells.
+// Also note that we always include at least a small component in each axes because in the past totally 'right-angled'
 //swipes weren't detected properly. But we were using a different approach to touch simulation then,
 //so this might now be unnecessary.
+#define BIG_RATIO (0.3)
+#define SMALL_RATIO (0.05)
+#define SWIPE_DURATION (0.1)
+
+//returns what portion of the view to swipe along in the x and y axes.
 CGSize swipeRatiosForDirection(NSString *direction){
-    static const CGFloat bigRatio = 0.3, smallRatio = 0.05;
     NSString *dir = [direction lowercaseString];
     
     if([dir isEqualToString:@"left"]){
-        return CGSizeMake(-bigRatio, smallRatio);
+        return CGSizeMake(-BIG_RATIO, SMALL_RATIO);
     }else if([dir isEqualToString:@"right"]){
-        return CGSizeMake(bigRatio, smallRatio);
+        return CGSizeMake(BIG_RATIO, SMALL_RATIO);
     }else if([dir isEqualToString:@"up"]){
-        return CGSizeMake(smallRatio, -bigRatio);
+        return CGSizeMake(SMALL_RATIO, -BIG_RATIO);
     }else if([dir isEqualToString:@"down"]){
-        return CGSizeMake(smallRatio, bigRatio);
+        return CGSizeMake(SMALL_RATIO, BIG_RATIO);
     }else{
         [NSException raise:@"invalid swipe direction" format:@"swipe direction '%@' is invalid", direction];
+        return CGSizeZero; // just here to stop the compiler whining.
     }
 }
-
-
-#define SWIPE_DURATION (0.1)
-CGSize swipeDeltasForDirection(NSString *direction); // defined in UIView+FrankGestures.m
 
 - (NSString *) FUI_swipe:(NSString *)dir{
     CGPoint swipeStart = centerOfViewInWindowCoords(self);
