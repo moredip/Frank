2359bfb7b6143ed794accd5b2a80849cc4faa318
Pete Hodgson <git@thepete.net>
2012-07-05 08:05:57 -0700

    Add example feature showing how to launch your app in iPhone and iPad idioms

--- example/Controls/features/step_definitions/launch_steps.rb
+++ example/Controls/features/step_definitions/launch_steps.rb
@@ -1,6 +1,16 @@
+def app_path
+  ENV['APP_BUNDLE_PATH'] || (defined?(APP_BUNDLE_PATH) && APP_BUNDLE_PATH)
+end
+
 Given /^I launch the app$/ do
   unless $USING_PHYSICAL_DEVICE
-    app_path = ENV['APP_BUNDLE_PATH'] || (defined?(APP_BUNDLE_PATH) && APP_BUNDLE_PATH)
     launch_app app_path unless ENV.key?('DONT_RELAUNCH')
+  end
+end
+
+Given /^I launch the app using the (iphone|ipad) simulator$/ do |idiom|
+  unless $USING_PHYSICAL_DEVICE
+    sdk = nil # request default SDK, which will be the most recent
+    launch_app app_path, nil, idiom
   end
 end
