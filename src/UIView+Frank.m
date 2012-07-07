cedf6271ab0aa01f124a072b4d31a408056a8b76
Pete Hodgson <git@thepete.net>
2012-07-07 14:51:35 -0600

    Add UIView#FEX_flash
    A bit better than KIF's flash (opacity goes in and out)

---
+++ src/UIView+Frank.m
@@ -0,0 +1,38 @@
+//
+//  UIView+Frank.m
+//  Frank
+//
+//  Created by Thoughtworks on 7/7/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+
+#import "LoadableCategory.h"
+
+MAKE_CATEGORIES_LOADABLE(UIView_Frank)
+
+@implementation UIView (Frank)
+
+// Based on UISpec's flash method
+- (void) FEX_flash{
+	UIColor *originalBackgroundColor = [self.backgroundColor retain];
+    CGFloat orginalAlpha = self.alpha;
+    for (NSUInteger i = 0; i < 5; i++) {
+        self.backgroundColor = [UIColor yellowColor];
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        
+        self.alpha = 0;
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        
+        self.backgroundColor = [UIColor blueColor];
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        
+        self.alpha = 1;
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+    }
+    self.alpha = orginalAlpha;
+    self.backgroundColor = originalBackgroundColor;
+    [originalBackgroundColor release];
+}
+
+@end
