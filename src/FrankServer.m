c40c4f70d9f86be2c953fcf740b534b6a7bc2864
Pete Hodgson <git@thepete.net>
2012-07-26 09:54:31 -0700

    Extract image capture into a separate route Conflicts:
    	Frank.xcodeproj/project.pbxproj
    	example/Controls/Controls.xcodeproj/project.pbxproj
    	lib/cocoahttpserver

--- src/FrankServer.m
+++ src/FrankServer.m
@@ -11,6 +11,7 @@
 #import "HTTPServer.h"
 #import "RoutingHTTPConnection.h"
 #import "StaticResourcesRoute.h"
+#import "ImageCaptureRoute.h"
 #import "FrankCommandRoute.h"
 #import "DumpCommand.h"
 #import "MapOperationCommand.h"
@@ -52,6 +53,9 @@ static NSUInteger __defaultPort = FRANK_SERVER_PORT;
 		
 		StaticResourcesRoute *staticRoute = [[[StaticResourcesRoute alloc] initWithStaticResourceSubDir:bundleName] autorelease];
 		[[RequestRouter singleton] registerRoute:staticRoute];
+        
+        ImageCaptureRoute *imageCaptureCommand = [[[ImageCaptureRoute alloc] init] autorelease];
+		[[RequestRouter singleton] registerRoute:imageCaptureCommand];
 		
 		_httpServer = [[[HTTPServer alloc]init] retain];
 		
---
+++ src/ImageCaptureRoute.h
@@ -0,0 +1,18 @@
+//
+//  ImageCaptureRoute.h
+//  Frank
+//
+//  Created by Pete Hodgson on 7/26/12.
+//  Copyright (c) 2012 Thoughtworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FrankCommandRoute.h"
+
+
+@interface ImageCaptureRoute : NSObject<Route> {
+}
+
+
+@end
