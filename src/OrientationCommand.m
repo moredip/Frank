8274f6c20c13f86c10781e4e8e85744f5df3a95e
Pete Hodgson <git@thepete.net>
2012-09-05 09:06:26 -0700

    add more detailed orientation reporting to Orientation command

--- src/OrientationCommand.m
+++ src/OrientationCommand.m
@@ -15,11 +15,46 @@
 
 @implementation OrientationCommand
 
-- (NSString *)getOrientationDescriptionViaStatusBar{
-    if( UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]) )
-        return @"portrait";
-    else
-        return @"landscape";
+- (NSDictionary *)representOrientation:(NSString *)orientation withDetailedOrientation:(NSString *)detailedOrientation{
+    return [NSDictionary dictionaryWithObjectsAndKeys:orientation,@"orientation", detailedOrientation,@"detailed_orientation",nil];
+}
+
+- (NSDictionary *)getOrientationRepresentationViaStatusBar{
+    switch([[UIApplication sharedApplication] statusBarOrientation]){
+		case UIInterfaceOrientationLandscapeLeft:
+            return [self representOrientation:@"landscape" withDetailedOrientation:@"landscape_left"];
+		case UIInterfaceOrientationLandscapeRight:
+            return [self representOrientation:@"landscape" withDetailedOrientation:@"landscape_right"];
+		case UIInterfaceOrientationPortrait:
+            return [self representOrientation:@"portrait" withDetailedOrientation:@"portrait"];
+		case UIInterfaceOrientationPortraitUpsideDown:
+            return [self representOrientation:@"portrait" withDetailedOrientation:@"portrait_upside_down"];
+        default:
+            NSLog(@"Device orientation via status bar is unknown");
+            return nil;
+    }
+}
+
+- (NSDictionary *)getOrientationRepresentationViaDevice{
+    switch ( [UIDevice currentDevice].orientation ) {
+		case UIDeviceOrientationLandscapeRight:
+            return [self representOrientation:@"landscape" withDetailedOrientation:@"landscape_right"];
+		case UIDeviceOrientationLandscapeLeft:
+            return [self representOrientation:@"landscape" withDetailedOrientation:@"landscape_right"];
+		case UIDeviceOrientationPortrait:
+            return [self representOrientation:@"portrait" withDetailedOrientation:@"portrait"];
+		case UIDeviceOrientationPortraitUpsideDown:
+            return [self representOrientation:@"portrait" withDetailedOrientation:@"portrait_upside_down"];
+        case UIDeviceOrientationFaceUp:
+            NSLog(@"Device orientation is face up");
+            return nil;
+        case UIDeviceOrientationFaceDown:
+            NSLog(@"Device orientation is face down");
+            return nil;
+        default:
+            NSLog(@"Device orientation via device is unknown");
+            return nil;
+	}
 }
 
 - (NSString *)getOrientationDescriptionViaDevice{
@@ -45,12 +80,11 @@
 }
 
 - (NSString *)handleGet{
-   	NSString *orientationDescription = [self getOrientationDescriptionViaDevice];
+   	NSDictionary *orientationDescription = [self getOrientationRepresentationViaDevice];
     if( !orientationDescription )
-        orientationDescription = [self getOrientationDescriptionViaStatusBar];
+        orientationDescription = [self getOrientationRepresentationViaDevice];
 	
-	NSDictionary *dom = [NSDictionary dictionaryWithObject:orientationDescription forKey:@"orientation"];
-	return [dom JSONRepresentation];
+	return [orientationDescription JSONRepresentation];
 }
 
 - (NSString *)handlePost:(NSString *)requestBody{
