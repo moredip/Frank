181bcff2f491324ce246ea7f213fff03158cee10
Pete Hodgson <git@thepete.net>
2012-09-15 09:06:42 -0700

    Allow size of swipe gesture to be configurable.
    Contribution courtesy of Casey Borders, via email.

--- src/UIView+FrankGestures.m
+++ src/UIView+FrankGestures.m
@@ -21,35 +21,39 @@ MAKE_CATEGORIES_LOADABLE(UIView_FrankGestures)
 
 CGSize swipeDeltasForDirection(NSString *direction);
 
-CGSize swipeDeltasForDirection(NSString *direction){
+CGSize swipeDeltasForDirection(NSString *direction, int pixels){
     NSString *dir = [direction lowercaseString];
 
     if([dir isEqualToString:@"left"]){
-        return CGSizeMake(-BIG_DELTA, SMALL_DELTA);
+        return CGSizeMake(-pixels, SMALL_DELTA);
     }else if([dir isEqualToString:@"right"]){
-        return CGSizeMake(BIG_DELTA, SMALL_DELTA);
+        return CGSizeMake(pixels, SMALL_DELTA);
     }else if([dir isEqualToString:@"up"]){
-        return CGSizeMake(SMALL_DELTA, -BIG_DELTA);
+        return CGSizeMake(SMALL_DELTA, -pixels);
     }else if([dir isEqualToString:@"down"]){
-        return CGSizeMake(SMALL_DELTA, BIG_DELTA);
+        return CGSizeMake(SMALL_DELTA, pixels);
     }else{
         [NSException raise:@"invalid swipe direction" format:@"swipe direction '%@' is invalid", direction];
     }
 }
 
 -(void)swipeInDirection:(NSString *)dir {
+    [self swipeInDirection:dir by:BIG_DELTA];
+}
+
+-(void)swipeInDirection:(NSString *)dir by:(int)pixels {
     CGPoint centerOfView = CGPointMake(self.frame.size.width * 0.5f, self.frame.size.height * 0.5f);
     CGPoint swipeStart = centerOfView;
-
-    CGSize delta = swipeDeltasForDirection(dir);
-
+    
+    CGSize delta = swipeDeltasForDirection(dir, pixels);
+    
     CGPoint swipePath[NUM_POINTS_IN_SWIPE_PATH];
     for( int i = 0; i < NUM_POINTS_IN_SWIPE_PATH; i++ ){
         CGFloat progress = ((CGFloat)i)/NUM_POINTS_IN_SWIPE_PATH;
-        swipePath[i] = CGPointMake( swipeStart.x + 
+        swipePath[i] = CGPointMake( swipeStart.x +
                                    (delta.width*progress), swipeStart.y+(delta.height*progress));
     }
-
+    
     [self dragAlongPathWithPoints:swipePath count:NUM_POINTS_IN_SWIPE_PATH];
 }
 
