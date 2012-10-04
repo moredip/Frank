c8bf04350466b0696ed3a2c2080fb3673ec47048
kra Larivain <olarivain@gmail.com>
2012-10-04 00:22:16 -0700

    - added Category on MKMapKit to scroll to a visible rect.

---
+++ src/MKMapView+FrankScrolling.h
@@ -0,0 +1,18 @@
+//
+//  MKMapView+FrankScrolling.h
+//  Frank
+//
+//  Created by Olivier Larivain on 10/4/12.
+//
+//
+#import <MapKit/MapKit.h>
+
+
+@interface MKMapView (FrankScrolling)
+
+- (void) setVisibleMapRectAtX: (double) x
+                            y: (double) y
+                        width: (double) width
+                       height: (double) height;
+
+@end
