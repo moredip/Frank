c4064b61fe682db194ae59d4e5befc3e206411f6
Pete Hodgson <git@thepete.net>
2012-10-04 17:12:40 -0700

    change extension method namespacing prefix from frank_ to FEX_

--- src/UIScrollView+FrankScrolling.m
+++ src/UIScrollView+FrankScrolling.m
@@ -6,34 +6,32 @@
 //
 //
 
-#import "UIScrollView+FrankScrolling.h"
-
 @implementation UIScrollView (FrankScrolling)
 
-- (void) frank_scrollToTop {
+- (void) FEX_scrollToTop {
     [self setContentOffset: CGPointZero];
     
-    [self frank_forceDelegateCallOnScroll];
+    [self FEX_forceDelegateCallOnScroll];
 }
 
-- (void) frank_scrollToBottom {
+- (void) FEX_scrollToBottom {
     CGPoint maxContentOffset = CGPointZero;
     maxContentOffset.y = self.contentSize.height - self.frame.size.height;
     [self setContentOffset: maxContentOffset];
     
-    [self frank_forceDelegateCallOnScroll];
+    [self FEX_forceDelegateCallOnScroll];
 }
 
 
-- (void) frank_setContentOffsetX: (NSInteger) x
+- (void) FEX_setContentOffsetX: (NSInteger) x
                          y: (NSInteger) y {
     CGPoint point = CGPointMake(x, y);
     [self setContentOffset: point animated: NO];
     
-    [self frank_forceDelegateCallOnScroll];
+    [self FEX_forceDelegateCallOnScroll];
 }
 
-- (void) frank_forceDelegateCallOnScroll {
+- (void) FEX_forceDelegateCallOnScroll {
     // force call to delegate, in case implementors rely on this behavior
     if([self.delegate respondsToSelector: @selector(scrollViewDidScroll:)]) {
         [self.delegate scrollViewDidScroll: self];
@@ -45,13 +43,13 @@
 
 @implementation UITableView (FrankScrolling)
 
-- (void) frank_scrollToRow: (NSInteger) row inSection: (NSInteger) section {
+- (void) FEX_scrollToRow: (NSInteger) row inSection: (NSInteger) section {
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow: row inSection: section];
     [self scrollToRowAtIndexPath: indexPath
                 atScrollPosition: UITableViewScrollPositionNone
                         animated: NO];
     
-    [self frank_forceDelegateCallOnScroll];
+    [self FEX_forceDelegateCallOnScroll];
 }
 
 @end
