e1a5dcae1b5cd4ca41a3fd261afb56c7fbc2ecee
Micke Lisinge <lisinge@gmail.com>
2012-10-30 09:29:11 +0100

    Added support for setting the location

---
+++ src/LocationCommand.h
@@ -0,0 +1,16 @@
+//
+//  LocationCommand.h
+//  Frank
+//
+//  Created by Micke Lisinge on 10/30/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "FrankCommandRoute.h"
+
+@interface LocationCommand : NSObject<FrankCommand> {
+    
+}
+
+@end
