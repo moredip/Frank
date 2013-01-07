735c561c8217bce4578090e2bd1eace5d4f8d8a3
Francis Chary <fchary@gmail.com>
2013-01-07 15:55:43 -0600

    Added a UIAlertView screen, including a Frank test for UIAlertView.

---
+++ example/Controls/Controls/AlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlertViewController.h
+//  Controls
+//
+//  Created by Francis Chary on 2013-01-07.
+//  Copyright (c) 2013 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlertViewController : UIViewController
+
+@end
