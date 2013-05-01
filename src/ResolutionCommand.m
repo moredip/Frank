797381ef3a9b240d3f101b78aabd7244b830ef66
Michael Buckley <thebuckley@gmail.com>
2013-04-30 21:35:54 -0700

    Pass screen scale to Symbiote along with device resolution

--- src/ResolutionCommand.m
+++ src/ResolutionCommand.m
@@ -17,20 +17,25 @@
 
 - (NSString *) handleCommandWithRequestBody:(NSString *) requestBody {
     
-    CGSize resolution = CGSizeMake(0, 0);
+    CGSize  resolution = CGSizeMake(0, 0);
+    CGFloat scale      = 0.0;
     
 #if TARGET_OS_IPHONE
-    CGFloat scale     = [[UIScreen mainScreen] scale];
+    scale             = [[UIScreen mainScreen] scale];
     resolution        = [[UIScreen mainScreen] bounds].size;
     resolution.width  = resolution.width * scale;
     resolution.height = resolution.height * scale;
 #else
     resolution = [[NSApplication sharedApplication] FEX_accessibilityFrame].size;
+    // TODO: Revisit to see if CGEvent and accessibility APIs on OS X have been
+    // updated to use points instead of pixels.
+    scale      = 1.0;
 #endif
     
     NSDictionary* response = [NSDictionary dictionaryWithObjectsAndKeys:
                               [NSNumber numberWithFloat: resolution.width],  @"width",
                               [NSNumber numberWithFloat: resolution.height], @"height",
+                              [NSNumber numberWithFloat: scale],             @"scale",
                               nil];
     
     return TO_JSON(response);
