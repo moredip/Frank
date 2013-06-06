14923f0090c4cf626f0d0d0a3439bd53e1608024
Ondrej Hanslik <ohanslik@vendavo.com>
2013-06-06 12:11:32 +0200

    Improved visibility checks

--- src/UIView+Frank.m
+++ src/UIView+Frank.m
@@ -14,41 +14,67 @@ MAKE_CATEGORIES_LOADABLE(UIView_Frank)
 @implementation UIView (Frank)
 
 // Based on UISpec's flash method
-- (void) FEX_flash{
+- (void)FEX_flash {
 	UIColor *originalBackgroundColor = [self.backgroundColor retain];
+    
     CGFloat orginalAlpha = self.alpha;
+    
     for (NSUInteger i = 0; i < 5; i++) {
         self.backgroundColor = [UIColor yellowColor];
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.05, false);
         
-        self.alpha = 0;
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        self.alpha = 0.0f;
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.05, false);
         
         self.backgroundColor = [UIColor blueColor];
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.05, false);
         
-        self.alpha = 1;
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, .05, false);
+        self.alpha = 1.0f;
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.05, false);
     }
+    
     self.alpha = orginalAlpha;
     self.backgroundColor = originalBackgroundColor;
+    
     [originalBackgroundColor release];
 }
 
-- (BOOL) FEX_isVisible{
-    if( [self isHidden] )
-        return false;
+- (BOOL)FEX_isVisible {
+    if (self.hidden || self.alpha < 0.01f) {
+        return NO;
+    }
     
-    if( [self superview] ){
-        return [[self superview] FEX_isVisible];
-    }else{
-        return true;
+    if (self.superview != nil) {
+        return [self.superview FEX_isVisible];
+    } else {
+        return YES;
     }
 }
 
 - (BOOL)FEX_isFullyWithinWindow {
     CGRect myFrameInWindowCoords = [self.window convertRect:self.bounds fromView:self];
     return CGRectContainsRect(self.window.bounds, myFrameInWindowCoords);
+}
+
+// checking if the view is fully visible in its superviews. We don't check if the view is overlapped by other views.
+- (BOOL)FEX_isFullyVisible {
+    if (![self FEX_isVisible] || ![self FEX_isFullyWithinWindow]) {
+        return NO;
+    }
+    
+    UIView* superview = self.superview;
+    
+    while (superview != nil) {
+        CGRect viewRect = [self convertRect:self.bounds toView:superview];
+        
+        if (superview.clipsToBounds && !CGRectContainsRect(superview.bounds, viewRect)) {
+            return NO;
+        }
+        
+        superview = superview.superview;
+    }
+    
+    return YES;
 }
 
 @end
