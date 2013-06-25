e6bfd03bd8f0756ab72fddc7f2d3f0e529940fef
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2013-06-25 21:46:33 +0900

    Implement bonjour and target option for frank console.

--- gem/lib/frank-cucumber/cli.rb
+++ gem/lib/frank-cucumber/cli.rb
@@ -201,6 +201,8 @@ module Frank
     end
 
     desc 'console', "launch a ruby console connected to your Frankified app"
+    method_option :bonjour, :type => :boolean, :default => false, :aliases => :b, :desc => "find Frank via Bonjour." 
+    method_option :target, :type => :string, :default => false, :aliases => :t, :desc => "target URL for Frank."
     def console
       # TODO: check whether app is running (using ps or similar), and launch it if it's not
 
@@ -214,6 +216,8 @@ module Frank
 
       Frank::Cucumber::FrankHelper.use_shelley_from_now_on
       console = Frank::Console.new
+      Frank::Cucumber::FrankHelper.test_on_physical_device_via_bonjour if options[:bonjour]
+      Frank::Cucumber::FrankHelper.server_base_url = options[:target] if options[:target]
       if console.check_for_running_app
         console.pry
       end
